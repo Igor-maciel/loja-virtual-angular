241938f2154d5eea0e1064ee9014155646821ac3
Igor-maciel <72346161+Igor-maciel@users.noreply.github.com>
2021-02-19 19:14:53 -0300

    configurando o projeot para gerar o html

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -5,10 +5,9 @@ import { ContatoComponent } from "./institucional/contato/contato.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 
 export const rooteRouterConfigu: Routes = [
-    {path:'', redirectTo: '/sobre',pathMatch:'full'},
-    {path:'', redirectTo: '/contato',pathMatch:'full'},
-    {path:'', redirectTo: '/home',pathMatch:'full'},
+
+   {path:'', redirectTo: '/home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
     {path:'contato',component:ContatoComponent},
-    {path:'contato',component:SobreComponent},
+    {path:'sobre',component:SobreComponent},
 ];
