b616e29b5b279c5921444b5bedcd27a0ed4bb277
Igor-maciel <72346161+Igor-maciel@users.noreply.github.com>
2021-02-18 21:43:45 -0300

    acresentando rotas

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,9 +1,14 @@
 
 import {Routes} from '@angular/router';
 import { HomeComponent } from "./navegacao/home.component";
-
+import { ContatoComponent } from "./institucional/contato/contato.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
 
 export const rooteRouterConfigu: Routes = [
+    {path:'', redirectTo: '/sobre',pathMatch:'full'},
+    {path:'', redirectTo: '/contato',pathMatch:'full'},
     {path:'', redirectTo: '/home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
+    {path:'contato',component:ContatoComponent},
+    {path:'contato',component:SobreComponent},
 ];
